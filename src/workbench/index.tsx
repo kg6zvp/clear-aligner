398d690cc10af60a6f197f0a42864b70c0991036
Sam McCollum <smccollum@mccollum.enterprises>
2023-12-04 23:49:37 +0000

    CA-1 - remove console errors

--- src/workbench/index.tsx
+++ src/workbench/index.tsx
@@ -15,71 +15,7 @@ interface WorkbenchProps {
 }
 
 const Workbench: React.FC<WorkbenchProps> = ({corpora, currentPosition}: WorkbenchProps): ReactElement => {
-  const [showSourceText] = React.useState(true);
-  const [showTargetText] = React.useState(true);
-  const [showLwcText] = React.useState(true);
-  const [showBackText] = React.useState(true);
-
   const [displayCorpora, setDisplayCorpora] = React.useState([] as Corpus[]);
-
-  React.useEffect(() => {
-    if (showSourceText) {
-      queryText('sbl', currentPosition)
-        .then(async (corpus) => {
-          if (corpus) {
-            const syntaxData = await fetchSyntaxData(currentPosition);
-            const sourceCorpus: Corpus = {
-              ...corpus,
-              syntax: {...syntaxData!, _syntaxType: SyntaxType.Source}
-            };
-            setDisplayCorpora([...displayCorpora, sourceCorpus]);
-          }
-        });
-    }
-  }, [showSourceText, displayCorpora, setDisplayCorpora, currentPosition]);
-
-  React.useEffect(() => {
-    if (showTargetText) {
-      queryText('nvi', currentPosition)
-        .then(async (corpus) => {
-          if (corpus) {
-            const syntaxData = await fetchSyntaxData(currentPosition);
-            const targetCorpus: Corpus = {
-              ...corpus,
-              syntax: {...syntaxData!, _syntaxType: SyntaxType.Mapped},
-            };
-            setDisplayCorpora([...displayCorpora, targetCorpus]);
-          }
-        });
-    }
-  }, [showTargetText, displayCorpora, setDisplayCorpora, currentPosition]);
-
-  React.useEffect(() => {
-    if (showLwcText) {
-      queryText('leb', currentPosition)
-        .then(async (corpus) => {
-          if (corpus) {
-            const syntaxData = await fetchSyntaxData(currentPosition);
-            const lwcCorpus: Corpus = {
-              ...corpus,
-              syntax: {...syntaxData!, _syntaxType: SyntaxType.MappedSecondary},
-            }
-            setDisplayCorpora([...displayCorpora, lwcCorpus]);
-          }
-        });
-    }
-  }, [showLwcText, displayCorpora, setDisplayCorpora, currentPosition])
-
-  React.useEffect(() => {
-    if (showBackText) {
-      queryText('backTrans', currentPosition)
-        .then(async (corpus) => {
-          if (corpus) {
-            setDisplayCorpora([...displayCorpora, corpus]);
-          }
-        });
-    }
-  }, [showBackText, displayCorpora, setDisplayCorpora, currentPosition]);
 
   return (<>
     {corpora &&
