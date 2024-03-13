1a137aca7a89deb2034231c42bc184b8879950f2
Sam McCollum <smccollum@mccollum.enterprises>
2024-03-13 15:01:05 +0000

    CA-63 - item #42

--- src/utils/useInitialization.ts
+++ src/utils/useInitialization.ts
@@ -1,22 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Project, ProjectTable } from '../state/projects/tableManager';
 import { UserPreference, UserPreferenceTable } from '../state/preferences/tableManager';
 import { ProjectState } from '../state/databaseManagement';
 import { LinksTable } from '../state/links/tableManager';
 import { AppContextProps } from '../App';
+import { useInterval } from 'usehooks-ts';
 
 const useInitialization = () => {
   const isLoaded = React.useRef(false);
   const [projects, setProjects] = React.useState<Project[]>([]);
   const [preferences, setPreferences] = React.useState<UserPreference | undefined>()
   const [state, setState] = useState({} as ProjectState);
+  const projectsIdList = useMemo<string>(() => JSON.stringify(projects.map(p => p.id).sort()), [projects]);
+
+  useInterval(() => {
+    state?.projectTable?.getProjects(true).then(res => {
+      const newProjectsList = [...res!.values() ].sort();
+      const newList = JSON.stringify(newProjectsList.map(p => p.id).sort());
+      if (newList !== projectsIdList) {
+        setProjects(newProjectsList);
+      }
+    });
+  }, 5_000);
 
   useEffect(() => {
     if(!isLoaded.current) {
       const currLinksTable = state.linksTable ?? new LinksTable();
       const currProjectTable = state.projectTable ?? new ProjectTable();
       const currUserPreferenceTable = state.userPreferenceTable ?? new UserPreferenceTable();
-
 
       setState({
         ...state,
