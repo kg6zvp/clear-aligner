f4f73fb8acd2b72390fb163c56cfc2bd3ac5341a
blentz <brendan.lentz@opsysinc.com>
2024-03-13 12:50:31 -0700

    CA-Item-22-v2 Fix Loader Theme Color
    This commit makes it so that the loader/busybox appearing at the top level of the app respects the theme coloring. This was done by moving that Dialog component into the AppLayout component, so it sits under the Themed Component (which controls those colors).

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -14,7 +14,7 @@ import {
   Toolbar,
   useMediaQuery,
   Typography,
-  Grid
+  Grid, DialogContent, Box, CircularProgress, DialogContentText, Dialog
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import React, { createContext, useContext, useMemo, useState } from 'react';
@@ -27,7 +27,12 @@ import {
 } from 'react-router-dom';
 import { AddLink, ManageSearch, LibraryBooks } from '@mui/icons-material';
 import useTrackLocation from './utils/useTrackLocation';
-import { AppContext } from './App';
+import { AppContext, AppContextProps } from './App';
+import { getCorporaInitializationState, InitializationStates } from './workbench/query';
+import { DatabaseStatus } from './state/links/tableManager';
+import { useInterval } from 'usehooks-ts';
+import _ from 'lodash';
+import useInitialization from './utils/useInitialization';
 
 type THEME = 'night' | 'day';
 type THEME_PREFERENCE = THEME | 'auto';
@@ -41,6 +46,10 @@ export interface LayoutContextProps {
 }
 
 export const LayoutContext = createContext({} as LayoutContextProps);
+
+// Needed for the Busybox/Dialog to work properly down below
+const BusyRefreshTimeInMs = 500;
+
 
 export const AppLayout = () => {
   useTrackLocation();
@@ -85,9 +94,95 @@ export const AppLayout = () => {
     (projects || []).find(p => p.id === preferences?.currentProject)?.name ?? projects?.[0]?.name ?? ""
   ), [projects, preferences]);
 
+  // code needed for the Dialog/BusyBox to work properly down below
+  const appContext: AppContextProps = useInitialization();
+  const [initializationState, setInitializationState] = useState<InitializationStates>();
+  const [databaseStatus, setDatabaseStatus] = useState<DatabaseStatus>();
+  useInterval(() => {
+    const newDatabaseStatus = appContext.projectState?.linksTable.getDatabaseStatus();
+    if (!_.isEqual(newDatabaseStatus, databaseStatus)) {
+      setDatabaseStatus(newDatabaseStatus);
+    }
+    const newInitializationState = getCorporaInitializationState();
+    if (newInitializationState !== initializationState) {
+      setInitializationState(newInitializationState);
+    }
+  }, BusyRefreshTimeInMs);
+  const spinnerParams = useMemo<{
+    isBusy?: boolean,
+    text?: string,
+    variant?: 'determinate' | 'indeterminate',
+    value?: number
+  }>(() => {
+    const busyInfo = databaseStatus?.busyInfo;
+    if (busyInfo?.isBusy) {
+      const progressCtr = busyInfo?.progressCtr ?? 0;
+      const progressMax = busyInfo?.progressMax ?? 0;
+      if (progressMax > 0
+        && progressMax >= progressCtr) {
+        const percentProgress = Math.round((progressCtr / progressMax) * 100.0);
+        return {
+          isBusy: true,
+          text: busyInfo?.userText ?? 'The database is busy...',
+          variant: percentProgress < 100 ? 'determinate' : 'indeterminate',
+          value: percentProgress < 100 ? percentProgress : undefined
+        };
+      } else {
+        return {
+          isBusy: true,
+          text: busyInfo?.userText ?? 'The database is busy...',
+          variant: 'indeterminate',
+          value: undefined
+        };
+      }
+    }
+    if (initializationState === InitializationStates.INITIALIZING) {
+      return {
+        isBusy: true,
+        text: 'Loading corpora...',
+        variant: 'indeterminate',
+        value: undefined
+      };
+    }
+    return {
+      isBusy: false,
+      text: undefined,
+      variant: 'indeterminate',
+      value: undefined
+    };
+  }, [databaseStatus?.busyInfo, initializationState]);
+
+
   return (
     <LayoutContext.Provider value={layoutContext}>
       <Themed theme={theme}>
+        <Dialog
+          open={!!spinnerParams.isBusy}>
+          <DialogContent>
+            <Box sx={{
+              display: 'flex',
+              margin: 'auto',
+              position: 'relative'
+            }}>
+              <CircularProgress sx={{ margin: 'auto' }}
+                                variant={spinnerParams.variant ?? 'indeterminate'}
+                                value={spinnerParams.value} />
+              {!!spinnerParams.value && <Box
+                sx={{
+                  position: 'absolute',
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)'
+                }}>
+                <Typography variant="caption">{`${spinnerParams.value}%`}</Typography>
+              </Box>}
+            </Box>
+            <DialogContentText>
+              {spinnerParams.text}
+            </DialogContentText>
+          </DialogContent>
+        </Dialog>
+
         <AppBar position={'fixed'} enableColorOnDark={theme !== 'night'}>
           <Grid container justifyContent="space-between" alignItems="center" sx={{position: 'relative'}}>
           <Toolbar aria-label={'Menu'} onClick={() => setShowMenu(!showMenu)}>
