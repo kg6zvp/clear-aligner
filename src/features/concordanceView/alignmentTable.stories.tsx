9285ab634b444de924ed4a9c6342a57d74c06af2
blentz <brendan.lentz@opsysinc.com>
2024-05-06 13:26:49 -0700

    CA-102 - Add StoryBook Story
    This commit adds a StoryBook story for testing.

--- src/features/concordanceView/alignmentTable.stories.tsx
+++ src/features/concordanceView/alignmentTable.stories.tsx
@@ -4,10 +4,14 @@
  */
 import { Meta } from '@storybook/react';
 import { AlignmentTable, AlignmentTableProps } from './alignmentTable';
+import BCVWP from '../bcvwp/BCVWPSupport';
+import { AlignmentSide, Link, TextDirection } from '../../structs';
+import { LocalizedWordEntry } from './structs';
+import { AppContext, AppContextProps } from '../../App';
 
 const meta: Meta<typeof AlignmentTable> = {
   title: 'Concordance View/AlignmentTable',
-  component: AlignmentTable,
+  component: AlignmentTable
 };
 
 export default meta;
@@ -20,5 +24,77 @@ export const Default = (props: AlignmentTableProps) => {
   );
 };
 
-Default.args = {
-} as AlignmentTableProps;
+Default.args = {} as AlignmentTableProps;
+
+interface MockedAlignmentTableProps extends AlignmentTableProps {
+  links: Link[],
+}
+
+export const CA102 = (props: MockedAlignmentTableProps) => {
+  // @ts-ignore
+  window.databaseApi = {
+    corporaGetLinksByAlignedWord: async (...args: any[]) => {
+      // @ts-ignore
+      console.log('args: ', args);
+      return (props.links);
+    }
+  };
+  return (
+    <AppContext.Provider value={{
+      containers: {
+        sourceContainer: undefined,
+        targetContainer: undefined,
+      }
+    } as AppContextProps} >
+      <AlignmentTable
+        {...props}
+      />
+    </AppContext.Provider>
+  );
+};
+CA102.args = {
+  wordSource: AlignmentSide.TARGET,
+  pivotWord: {
+    normalizedText: 'god',
+    side: AlignmentSide.TARGET,
+    frequency: 3,
+    languageInfo: {
+      code: 'eng',
+      textDirection: TextDirection.LTR
+    }
+  },
+  alignedWord: {
+    id: '123412345',
+    frequency: 5,
+    sourceWordTexts: {
+      text: 'Βίβλος',
+      languageInfo: {
+        code: 'grc',
+        textDirection: TextDirection.LTR
+      }
+    },
+    targetWordTexts: {
+      text: 'roll',
+      languageInfo: {
+        code: 'eng',
+        textDirection: TextDirection.LTR
+      }
+    }
+  },
+  links: [{
+    id: 'asdfasdf',
+    sources: [new BCVWP(1, 1, 1, 1, 1).toReferenceString()],
+    targets: [new BCVWP(1, 1, 1, 3, 1).toReferenceString()]
+  }, {
+    'id': 'ceeb33a7-31fe-41a0-993c-d92a1415232e',
+    'sources': [
+      '010010010021'
+    ],
+    'targets': [
+      '01001001003'
+    ]
+  }
+  ] as Link[]
+} as MockedAlignmentTableProps;
+
+
