3823ac89f73b672d89454bf7d8516c1fff57d42a
blentz <brendan.lentz@opsysinc.com>
2024-06-18 15:24:47 -0700

    CA-115 Add Toasts and Profile Menu

--- src/features/profileAvatar/profileAvatar.tsx
+++ src/features/profileAvatar/profileAvatar.tsx
@@ -1,58 +1,161 @@
-import { Avatar } from '@mui/material';
+import { Avatar, Button, IconButton, Menu, MenuItem } from '@mui/material';
 import { Person } from '@mui/icons-material';
+import CloseIcon from '@mui/icons-material/Close'
 import { styled } from '@mui/material/styles';
 import Badge from '@mui/material/Badge';
+import React, { useEffect } from 'react';
+import {useNetworkState} from "@uidotdev/usehooks"
+import Snackbar from '@mui/material/Snackbar';
 
+const userState = {
+  LoggedIn: {
+    color:  'green',
+    label: 'Logged In'
+  },
+  LoggedOut: {
+    color: 'red',
+    label: 'Logged Out',
+  },
+  Offline:{
+    color: 'grey',
+    label: 'Offline'
+  }
+}
 
-const StyledBadge = styled(Badge)(({ theme }) => ({
-  '& .MuiBadge-badge': {
-    backgroundColor: '#44b700',
-    color: '#44b700',
-    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-    '&::after': {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      borderRadius: '50%',
-      animation: 'ripple 1.2s infinite ease-in-out',
-      border: '1px solid currentColor',
-      content: '""',
-    },
-  },
-  '@keyframes ripple': {
-    '0%': {
-      transform: 'scale(.8)',
-      opacity: 1,
-    },
-    '100%': {
-      transform: 'scale(2.4)',
-      opacity: 0,
-    },
-  },
-}));
+const ProfileMenu = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        <Person/>
+      </Button>
+
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+}
+
 
 /**
  * This component is used for users to login/logout
  * and do things like access personal preferences.
  */
 export const ProfileAvatar = () => {
+  const [userStatus, setUserStatus] = React.useState(userState.LoggedIn)
+  const network = useNetworkState();
+  const [isSnackBarOpen, setIsSnackBarOpen] = React.useState(false)
+  const [snackBarMessage, setSnackBarMessage] = React.useState("")
+
+  useEffect( () => {
+    console.log('inside useEffect')
+    if(network.online){
+      console.log('inside network.online')
+      setUserStatus(userState.LoggedOut)
+      setSnackBarMessage('Internet Connection Detected.')
+      setIsSnackBarOpen(true)
+
+    }
+    else{
+      console.log('inside the else')
+      setSnackBarMessage('No internet connection.')
+      setIsSnackBarOpen(true)
+      setUserStatus(userState.Offline)
+    }
+  },[network])
+
+  const handleCloseSnackbar = (event: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setIsSnackBarOpen(false);
+  };
+
+  const action = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleCloseSnackbar}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+      backgroundColor: userStatus.color,
+      color: userStatus.color,
+      boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+      '&::after': {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        borderRadius: '50%',
+        animation: 'ripple 1.2s infinite ease-in-out',
+        border: '1px solid currentColor',
+        content: '""',
+      },
+    },
+  }));
 
   return(
-    <StyledBadge
-      overlap="circular"
-      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-      variant="dot"
-    >
-      <Avatar
-        sx={{
-          margin: 2,
-          height: 30,
-          width: 30,
-        }}
+    <>
+        <StyledBadge
+          overlap="circular"
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+          variant="dot"
+          sx={{
+            margin: 2.3,
+          }}
+        >
+            <Avatar
+              sx={{
+                height: 27,
+                width: 27,
+              }}
+            >
+              <ProfileMenu/>
+            </Avatar>
+        </StyledBadge>
+      <Snackbar
+        open={isSnackBarOpen}
+        autoHideDuration={5000}
+        onClose={handleCloseSnackbar}
+        message={snackBarMessage}
+        action={action}
       />
-    </StyledBadge>
+    </>
 
   )
 }
