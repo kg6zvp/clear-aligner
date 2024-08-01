02a8820df6258c0f1ba5365d2e218d930762e86e
blentz <brendan.lentz@opsysinc.com>
2024-08-01 10:54:06 -0700

    CA-139 Initial Commit
    This commit adds the initial menu required for this story. A user can right click on a gloss and get this menu to pop up.

--- src/features/textSegment/glossSegment.tsx
+++ src/features/textSegment/glossSegment.tsx
@@ -4,11 +4,15 @@
  */
 import React from 'react';
 import { ThemeMode } from '../themed';
-import { Box, Divider, Grid, Paper } from '@mui/material';
+import { Box, Divider, Grid, Menu, MenuItem, Paper, useTheme } from '@mui/material';
 import { Corpus, LanguageInfo, Link, Word } from '../../structs';
 import TextSegment from './index';
 import { LocalizedTextDisplay } from '../localizedTextDisplay';
 import { LimitedToLinks } from '../corpus/verseDisplay';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { Cancel, CheckCircle, Flag } from '@mui/icons-material';
+import LinkIcon from '@mui/icons-material/Link';
 
 interface GlossSegmentProps extends LimitedToLinks {
   readonly?: boolean;
@@ -38,61 +42,140 @@ const GlossSegment: React.FC<GlossSegmentProps> = ({
                         parts,
                         languageInfo
                       }: GlossSegmentProps) => {
+
+  const [isLinkStateMenuOpen, setIsLinkStateMenuOpen] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleRightClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setAnchorEl(event.currentTarget);
+    setIsLinkStateMenuOpen(true);
+  }
+  const handleClose = () => {
+    setAnchorEl(null);
+    setIsLinkStateMenuOpen(false);
+  };
+  const handleMenuClick = () => {
+    setAnchorEl(null);
+    setIsLinkStateMenuOpen(false);
+  }
+
+  const theme = useTheme();
+
   return (
-    <Paper variant="outlined" sx={theme => ({
-      display: 'inline-block',
-      p: 1,
-      m: .25,
-      borderColor:  theme.palette.tokenButtons.defaultTokenButtons.outline,
-      ...(theme.palette.mode === ThemeMode.DARK ? {
-        background: 'transparent'
-      } : {})
-    })}>
-      <Grid container>
-        {
-          (parts || []).map((wordPart: Word, idx: number) => {
-            return (
-              <React.Fragment key={wordPart.id}>
-                <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
-                  <TextSegment
-                    key={wordPart.id}
-                    readonly={readonly}
-                    onlyLinkIds={onlyLinkIds}
-                    word={wordPart}
-                    links={links}
-                    languageInfo={languageInfo}
-                    disableHighlighting={disableHighlighting}
-                    showAfter={!suppressAfter}
-                    alignment={idx === 0 && (parts || []).length > 1 ? 'flex-end' : 'flex-start'}
-                  />
-                  <Grid container justifyContent={idx === 0 && (parts || []).length > 1 ? 'flex-end' : 'flex-start'} sx={{ height: '20px' }}>
-                    <LocalizedTextDisplay
+    <>
+      <Paper
+        onContextMenu={event => handleRightClick(event)}
+        variant="outlined"
+        sx={theme => ({
+        display: 'inline-block',
+        p: 1,
+        m: .25,
+        borderColor:  theme.palette.tokenButtons.defaultTokenButtons.outline,
+        ...(theme.palette.mode === ThemeMode.DARK ? {
+          background: 'transparent'
+        } : {})
+      })}>
+        <Grid container>
+          {
+            (parts || []).map((wordPart: Word, idx: number) => {
+              return (
+                <React.Fragment key={wordPart.id}>
+                  <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
+                    <TextSegment
+                      key={wordPart.id}
+                      readonly={readonly}
+                      onlyLinkIds={onlyLinkIds}
+                      word={wordPart}
+                      links={links}
                       languageInfo={languageInfo}
-                      variant="caption"
-                      sx={theme => ({
-                        color: theme.palette.tokenButtons.defaultTokenButtons.text
-                      })}
-                    >
-                      {wordPart.gloss || "-"}
-                    </LocalizedTextDisplay>
-                  </Grid>
-                </Box>
-                {
-                  idx !== ((parts || []).length - 1) && (
-                    <Divider flexItem orientation="vertical" sx={theme => ({
-                      borderStyle: 'dashed',
-                      borderWidth: '1px',
-                      mx: .5,
-                      borderColor:  theme.palette.tokenButtons.defaultTokenButtons.outline,
-                    })} />
-                  )
-                }
-              </React.Fragment>
-            )
-          })
-        }
-      </Grid>
-    </Paper>
+                      disableHighlighting={disableHighlighting}
+                      showAfter={!suppressAfter}
+                      alignment={idx === 0 && (parts || []).length > 1 ? 'flex-end' : 'flex-start'}
+                    />
+                    <Grid container justifyContent={idx === 0 && (parts || []).length > 1 ? 'flex-end' : 'flex-start'} sx={{ height: '20px' }}>
+                      <LocalizedTextDisplay
+                        languageInfo={languageInfo}
+                        variant="caption"
+                        sx={theme => ({
+                          color: theme.palette.tokenButtons.defaultTokenButtons.text
+                        })}
+                      >
+                        {wordPart.gloss || "-"}
+                      </LocalizedTextDisplay>
+                    </Grid>
+                  </Box>
+                  {
+                    idx !== ((parts || []).length - 1) && (
+                      <Divider flexItem orientation="vertical" sx={theme => ({
+                        borderStyle: 'dashed',
+                        borderWidth: '1px',
+                        mx: .5,
+                        borderColor:  theme.palette.tokenButtons.defaultTokenButtons.outline,
+                      })} />
+                    )
+                  }
+                </React.Fragment>
+              )
+            })
+          }
+        </Grid>
+      </Paper>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={isLinkStateMenuOpen}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem
+            onClick={handleMenuClick}
+        >
+          <ListItemIcon >
+            <LinkIcon sx={{
+              color : theme.palette.primary.main
+            }} />
+          </ListItemIcon>
+            Aligned
+        </MenuItem>
+        <MenuItem
+          onClick={handleMenuClick}
+        >
+          <ListItemIcon >
+            <Cancel sx={{
+              color : theme.palette.error.main
+            }} />
+          </ListItemIcon>
+          Rejected
+        </MenuItem>
+        <MenuItem
+          onClick={handleMenuClick}
+        >
+          <ListItemIcon>
+            <CheckCircle sx={{
+              color : theme.palette.success.main
+            }}/>
+          </ListItemIcon>
+          Approved
+        </MenuItem>
+        <MenuItem
+          onClick={handleMenuClick}
+        >
+          <ListItemIcon>
+            <Flag sx={{
+              color : theme.palette.warning.main
+            }} />
+          </ListItemIcon>
+          Flagged
+        </MenuItem>
+      </Menu>
+    </>
   );
 }
 export default GlossSegment;
