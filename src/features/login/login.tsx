3b6f363b98932bc8e783c6c317ac46f6faa59b53
blentz <brendan.lentz@opsysinc.com>
2024-08-20 16:47:57 -0700

    CA-154 Initial Commit

--- src/features/login/login.tsx
+++ src/features/login/login.tsx
@@ -3,12 +3,13 @@
  */
 import React, { ReactElement, useContext } from 'react';
 import { Box } from '@mui/system';
-import { Button, DialogTitle, Popover, Stack, Typography, useTheme } from '@mui/material';
+import { Button, DialogTitle, Link, Popover, Stack, Typography, useTheme } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import { signIn } from "aws-amplify/auth";
 import { userState } from '../profileAvatar/profileAvatar';
 import { AppContext } from '../../App';
 import LogoutIcon from '@mui/icons-material/Logout';
+import { useLocation } from 'react-router-dom';
 
 
 interface LoginProps {
@@ -34,23 +35,36 @@ export const Login:React.FC<LoginProps> = ({isLoginModalOpen,
   const [password, setPassword] = React.useState("")
   const theme = useTheme();
   const {setIsSnackBarOpen, setSnackBarMessage } = useContext(AppContext)
+  const [showPasswordResetURL, setShowPasswordResetURL] = React.useState(false);
+
+  const location = useLocation();
 
   const handleLogin = async() => {
+    setShowLoginError(false);
+    setShowPasswordResetURL(false);
     try{
-      await signIn({
+      const signInResponse = await signIn({
         username: emailAddress,
         password: password,
       })
-      setUserStatus(userState.LoggedIn);
-      setShowLoginError(false)
-      setSnackBarMessage("Signed in to ClearAligner Sync.")
-      setIsSnackBarOpen(true);
+      console.log('signInResponse is: ', signInResponse)
+
+      if (signInResponse.nextStep?.signInStep ===
+        "CONFIRM_SIGN_IN_WITH_NEW_PASSWORD_REQUIRED"){
+        setUserStatus(userState.LoggedOut);
+        setShowPasswordResetURL(true);
+      }
+      else{
+        setUserStatus(userState.LoggedIn);
+        setShowLoginError(false)
+        setSnackBarMessage("Signed in to ClearAligner Sync.")
+        setIsSnackBarOpen(true);
+      }
     }
     catch (error){
       console.log('error signing in: ', error)
       setShowLoginError(true)
     }
-
   }
 
   return (
@@ -126,6 +140,18 @@ export const Login:React.FC<LoginProps> = ({isLoginModalOpen,
                   }}
                 >
                   Incorrect email address or password.
+                </Typography>
+              }
+              {showPasswordResetURL &&
+                <Typography
+                  color={'error'}
+                  fontSize={'small'}
+                  sx={{
+                    mt: 1
+                  }}
+                >
+                  Please <Link href={`https://clear-aligner-2.auth.us-east-1.amazoncognito.com/login?client_id=jteqgoa1rgptil2tdi7b0nqjb&response_type=code&scope=aws.cognito.signin.user.admin+openid&redirect_uri=http://localhost:3000${location.pathname}`}>
+                  reset your password </Link> before signing in.
                 </Typography>
               }
             </Stack>
